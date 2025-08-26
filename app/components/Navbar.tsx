b60ac0366d787c390b39d7bd6ee63affb8a1d70f
jnannesh reddy <jnaneshreddyv730@gmail.com>
2025-08-26 22:44:54 +0530

    updated nav

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,10 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <Link to="/">
-                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+                <p className="text-2xl font-bold text-gradient leading-none whitespace-nowrap">
+                    RESUMIND
+                    <sup className="ml-1 text-[10px] font-medium text-white/70v text-gradient">by sansai</sup>
+                </p>
             </Link>
             <Link to="/upload" className="primary-button w-fit">
                 Upload Resume
